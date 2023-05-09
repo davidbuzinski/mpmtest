fd94c9958a060d8c35644c244ef48d29d842ab1d
David Buzinski <davidb@mathworks.com>
2023-05-09 14:44:58 -0400

    nogpu

--- src/mpm.ts
+++ src/mpm.ts
@@ -55,6 +55,9 @@ export async function install(mpmPath: string, release: matlab.Release, products
         `--destination=${destination}`,
         "--products",
     ]
+    if (release.name >= "r2023a") {
+        mpmArguments = mpmArguments.concat("--no-gpu");
+    }
     mpmArguments = mpmArguments.concat(parsedProducts);
 
     const exitCode = await exec.exec(mpmPath, mpmArguments);
